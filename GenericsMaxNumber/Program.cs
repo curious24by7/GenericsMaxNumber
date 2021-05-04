e3f809aedbcbf766bf16fbd7416f369b2efc5221
sohamdhamnaskar <sohamdham123@gmail.com>
2021-05-04 18:17:40 +0530

    [soham]Refactored the code by generic method,

--- GenericsMaxNumber/Program.cs
+++ GenericsMaxNumber/Program.cs
@@ -10,16 +10,22 @@ namespace GenericsMaxNumber
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("The Max Number in integer is ::");
-            int Output = MaxNumCheck.MaxIntNum(10, 20, 30);
-            Console.WriteLine(Output);
-            Console.WriteLine("The Max Number in Float is ::");
-            double DoubleOutput = MaxNumCheck.MaxFloatNum(50.2, 30.9, 60.50);
-            Console.WriteLine(DoubleOutput);
-            Console.WriteLine("The Max Number in String is ::");
-            string StringOP = MaxNumCheck.MaxStringNum("50", "110", "111");
-            Console.WriteLine(StringOP);
+            int[] intArray = { 10, 20, 30 };
+            double[] doubleArray = { 50.2, 30.9, 60.50 };
+            string[] stringArray = {"50", "110", "111"};
+
+            Program.MaxNum<int>(intArray);
+            Program.MaxNum<double>(doubleArray);
+            Program.MaxNum<string>(stringArray);
+
+
             Console.ReadKey();
+        }
+        public static void MaxNum<T>(T[] inputArray)
+        {
+            var m = inputArray.Max();
+            Console.WriteLine(m);
+            Console.WriteLine("------------------------");
         }
     }
 }
