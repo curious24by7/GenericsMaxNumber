e1aa4149a5a75c73695620c18313b7945455e5ec
sohamdhamnaskar <sohamdham123@gmail.com>
2021-05-04 15:55:34 +0530

    [soham]Added Find Max Float

--- GenericsMaxNumber/MaxNumCheck.cs
+++ GenericsMaxNumber/MaxNumCheck.cs
@@ -24,5 +24,21 @@ namespace GenericsMaxNumber
             }
             throw new Exception("Any two Numbers are equal.");
         }
+        public static double MaxFloatNum(double firstValue, double secondValue, double thirdValue)
+        {
+            if (firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0)
+            {
+                return firstValue;
+            }
+            if (secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) > 0)
+            {
+                return secondValue;
+            }
+            if (thirdValue.CompareTo(firstValue) > 0 && thirdValue.CompareTo(secondValue) > 0)
+            {
+                return thirdValue;
+            }
+            throw new Exception("Any two Numbers are equal.");
+        }
     }
 }
