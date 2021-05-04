76a4610d048ae3297f3bb87b5fbebbb492f3ee22
sohamdhamnaskar <sohamdham123@gmail.com>
2021-05-04 16:34:57 +0530

    [soham]Added Find Max Float

--- GenericsMaxNumber/MaxNumCheck.cs
+++ GenericsMaxNumber/MaxNumCheck.cs
@@ -40,5 +40,41 @@ namespace GenericsMaxNumber
             }
             throw new Exception("Any two Numbers are equal.");
         }
+        public static string MaxStringNum(string firstValue, string secondValue, string thirdValue)
+        {
+            if (firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0)
+            {
+                return firstValue;
+            }
+            if (secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) > 0)
+            {
+                return secondValue;
+            }
+            if (thirdValue.CompareTo(firstValue) > 0 && thirdValue.CompareTo(secondValue) > 0)
+            {
+                return thirdValue;
+            }
+            throw new Exception("Any two Numbers are equal.");
+        }
+        //    if (firstString.CompareTo(secondString) > 0 && firstString.CompareTo(thirdString) > 0 ||
+        //        firstString.CompareTo(secondString) >= 0 && firstString.CompareTo(thirdString) > 0 ||
+        //        firstString.CompareTo(secondString) > 0 && firstString.CompareTo(thirdString) >= 0)
+        //    {
+        //        return firstString;
+        //    }
+        //    if (secondString.CompareTo(firstString) > 0 && secondString.CompareTo(thirdString) > 0 ||
+        //        secondString.CompareTo(firstString) >= 0 && secondString.CompareTo(thirdString) > 0 ||
+        //        secondString.CompareTo(firstString) > 0 && secondString.CompareTo(thirdString) >= 0)
+        //    {
+        //        return secondString;
+        //    }
+        //    if (thirdString.CompareTo(firstString) > 0 && thirdString.CompareTo(secondString) > 0 ||
+        //        thirdString.CompareTo(firstString) >= 0 && thirdString.CompareTo(secondString) > 0 ||
+        //        thirdString.CompareTo(firstString) > 0 && thirdString.CompareTo(secondString) >= 0)
+        //    {
+        //        return thirdString;
+        //    }
+        //    throw new Exception("Any two Numbers are equal.");
+        //}
     }
 }
